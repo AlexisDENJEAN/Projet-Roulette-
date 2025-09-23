9f6401e8d5312ea3f35bf888266d6ab05e14c4e7
pizzaalonion <ben.mazzega@gmail.com>
2025-09-23 13:40:28 +0100

    MAJ

--- src/bet.ts
+++ src/bet.ts
@@ -1,1 +1,16 @@
-    //fichier pour les mises/bets
+        export type BetType = "number" | "color" | "evenOdd" | "highLow";
+
+    export interface Bet {
+        type: BetType;
+        value: number | "red" | "black" | "green" | "even" | "odd" | "high" | "low";
+        amount: number;
+    }
+
+    class BetValidator {
+        static validate(bet: Bet): boolean {
+            if (bet.type === "number") return bet.amount >= 1 && bet.amount <= 10;
+            return false;
+        }
+    }
+
+    module.exports = { BetValidator };
--- src/roulette.ts
+++ src/roulette.ts
@@ -8,11 +8,26 @@ export interface SpinResult {
     isHigh: boolean; // 19-36
     isLow: boolean;  // 1-18
 }
-export class Roulette {
+class Roulette {
     private numbers: number[] = Array.from({ length: 37 }, (_, i) => i); // 0 to 36
 
     private rednumbers: number[] = [
         1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36
-    ];  
-    
-    spin(): SpinResult {
+];      
+
+spin(): SpinResult { 
+    const number = this.numbers[Math.floor(Math.random() * this.numbers.length)]!;
+    let color: color;
+    if (number === 0) color = "green";
+    else if (this.rednumbers.includes(number)) color = "red";
+    else color = "black";
+    return {
+        number,
+        color,
+        isEven: number !== 0 && number % 2 === 0,
+        isOdd: number % 2 === 1,
+        isHigh: number >= 19 && number <= 36,
+        isLow: number >= 1 && number <= 18
+    };
+}
+}
